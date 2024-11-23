033d2c9bcd772a2377f0597d597cef71c22edab9
Mohammad Ayub <5043ayub@gmail.com>
2024-11-24 01:32:28 +0600

    Addeded recipes and designed the UI

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import { AiOutlineFire } from 'react-icons/ai';
+import { CiClock2 } from 'react-icons/ci';
 
 const Recipe = ({recipe}) => {
     const {id, name, img, calories, description, preparing_time, ingredients} = recipe;
     console.log(recipe)
   return (
-    <div className='border-2 p-2 rounded-md'>
-        <img src={img} alt="" />
-        <h1>{name}</h1>
+    <div className='border-2 p-3 rounded-md'>
+        <img className='rounded-md mb-2' src={img} alt="" />
+        <h3 className='text-xl mb-2 font-bold'>{name}</h3>
+        <p className='text-[14px] pb-3 mb-3 border-b-2'>{description}</p>
+        
+        <h4 className='font-semibold text-[17px]'>Ingredients: {ingredients.length}</h4>
+        <div className='pb-3 mb-3 border-b-2'>
+        {
+            ingredients.map(ingredient => <li>{ingredient}</li>)
+        }
+        </div>
+        <div className='flex justify-between items-center mb-3'>
+            <div className='flex justify-center items-center'>
+                <CiClock2 className='mr-2'></CiClock2>
+                <p><span>{preparing_time}</span> Minutes</p>
+            </div>
+            <div className='flex justify-center items-center'>
+                <AiOutlineFire className='mr-2'></AiOutlineFire>
+                <p><span>{calories}</span> Kcal</p>
+            </div>
+        </div>
+        <button className='bg-[#0BE58A] px-3 py-2 rounded-full font-semibold border-[#0BE58A] border-2 text-[13px]'>Want to Cook</button>
     </div>
   )
 }
--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -9,7 +9,7 @@ const Recipes = () => {
         .then(data => setRecipes(data))
     },[])
   return (
-    <div className='grid grid-cols-3 gap-4 w-2/3'>
+    <div className='grid grid-cols-2 gap-4 w-2/3 mb-16'>
         {
             recipes.map(recipe => <Recipe key={recipe.id} recipe={recipe}></Recipe>)
         }
